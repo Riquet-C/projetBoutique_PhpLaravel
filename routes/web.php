372abfaad1aa5b3ce91180e1dd72f23484ed9f66
Camille Riquet <camille.riquet@le-campus-numerique.fr>
2024-07-29 15:39:23 +0200

    add route product, product id, cart

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Homepage";
 });
+
+Route::get('/product', function () {
+    return "Liste des produits";
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit $id";
+});
+
+Route::get('/cart', function () {
+    return "Panier";
+});
+
