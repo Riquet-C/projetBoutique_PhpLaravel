43c07258b378ecee45cfef630af1c576617d84fd
Lucasrdh <lucaschretien38@hotmail.fr>
2024-07-31 12:20:21 +0200

    premier essai layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
+<body class="min-vh-100">
 <header>
     <!-- resources/views/partials/header.blade.php -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -31,15 +31,17 @@
 
 </header>
 
-
-<body class="min-vh-100">
 {{$content}}
-</body>
-
 
 <footer>
     <h3>Condition générale</h3>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </footer>
+
+
+</body>
 </html>
 
 
