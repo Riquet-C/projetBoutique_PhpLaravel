9a063e0e2fcaf398b02a3b45cceab8424e031260
OlivierMOULIN <olivier.moulin@le-campus-numerique.fr>
2024-08-06 09:41:13 +0200

    backoffice ajout delete et debut update

---
+++ resources/views/product-list.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot name="title">Catalogue</x-slot>
+    <x-slot name="content">
+
+{{-- @php(var_dump($product))--}}
+<div class="container d-flex justify-content-around">
+        @foreach($product as $value)
+<x-product
+        name="{{$value->name}}"
+{{--        prixht="500"--}}
+        prix="{{$value->price}}"
+        reduc="10%"
+{{--        newprix="250000"--}}
+        poid="{{$value->weight}}"
+        clean="{{$value->clean}}"
+        imageURL="{{$value->picture}}"
+        id="{{$value->id}}"
+/>
+        @endforeach
+</div>
+    </x-slot>
+</x-layout>
